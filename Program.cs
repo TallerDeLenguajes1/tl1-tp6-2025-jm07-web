d06f751a379c25833bb55ce1b8121e952eca74e4
Julieta Mariano <julimariano843@gmail.com>
2025-05-25 18:53:48 -0300

    Creo la rama CalculadoraV1 y hago el ejercicio 2

--- Program.cs
+++ Program.cs
@@ -44,8 +44,66 @@ if (numero > 0)
     }
 
     Console.WriteLine("El numero invertido es: " + invertido);
-} else
+}
+else
 {
     Console.WriteLine("Debe ingresar un número positivo para obtener el invertido.");
+}
+
+//Ejercicio 2
+int continuar = 1, operacion;
+float num1, num2, resultado = 0;
+string String_operacion, Snum1, Snum2;
+
+while (continuar != 0)
+{
+    do
+    {
+        Console.WriteLine("\nMenú de Operaciones: ");
+        Console.WriteLine("  1. Sumar | 2. Restar | 3. Multiplicar | 4. Dividir \n");
+        Console.WriteLine("Eleja una opción: ");
+        String_operacion = Console.ReadLine();
+        int.TryParse(String_operacion, out operacion);
+    } while (operacion < 1 || operacion > 4);
+
+    do
+    {
+        Console.WriteLine("\nIngrese el primer numero: ");
+        Snum1 = Console.ReadLine();
+        Console.WriteLine("\nIngrese el segundo numero: ");
+        Snum2 = Console.ReadLine();
+    } while (string.IsNullOrWhiteSpace(Snum1) || string.IsNullOrWhiteSpace(Snum2) || !float.TryParse(Snum1, out num1) || !float.TryParse(Snum2, out num2));
+
+    switch (operacion)
+    {
+        case 1:
+            resultado = num1 + num2;
+            Console.WriteLine($"\nResultado: {resultado}");
+            break;
+        case 2:
+            resultado = num1 - num2;
+            Console.WriteLine($"\nResultado: {resultado}");
+            break;
+        case 3:
+            resultado = num1 * num2;
+            break;
+        case 4:
+            if (num2 != 0)
+            {
+                resultado = num1 / num2;
+                Console.WriteLine($"\nResultado: {resultado}");
+            }
+            else
+            {
+                Console.WriteLine("\nNo se puede dividir por 0.");
+            }
+            break;
+        default:
+            Console.WriteLine("\nNo se encontró esa operación.");
+            continue;
+    }
+
+    Console.WriteLine("\nQuiere continuar? (SI = 1, NO = 0): ");
+    int.TryParse(Console.ReadLine(), out continuar);
 }
 
